e94545f134617bd5213e53fe6ae6035ebf91d4a4
Felipe Navarro <lipeandres@gmail.com>
2012-09-05 21:16:56 -0500

    .35 kernel with ssd1963 support (working), touch panel support (partial, framebuffer off 180 degrees)

---
+++ linux-2.6.35.3-17042011-km233/include/generated/compile.h
@@ -0,0 +1,9 @@
+/* This file is auto generated, version 91 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#91 PREEMPT Wed Sep 5 18:02:14 COT 2012"
+#define LINUX_COMPILE_TIME "18:02:14"
+#define LINUX_COMPILE_BY "carto"
+#define LINUX_COMPILE_HOST "carto-laptop"
+#define LINUX_COMPILE_DOMAIN "(none)"
+#define LINUX_COMPILER "gcc version 4.3.2 (Sourcery G++ Lite 2008q3-72) "
