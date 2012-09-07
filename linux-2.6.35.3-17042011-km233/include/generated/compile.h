6cdaa2a39f8a920f39a31558366ab0e3d51d0b84
Felipe Navarro <lipeandres@gmail.com>
2012-09-07 16:21:07 -0500

    deleted faulty kernel

--- linux-2.6.35.3-17042011-km233/include/generated/compile.h
+++
@@ -1,9 +0,0 @@
-/* This file is auto generated, version 104 */
-/*  PREEMPT */
-#define UTS_MACHINE "arm"
-#define UTS_VERSION "#104 PREEMPT Thu Sep 6 21:18:50 COT 2012"
-#define LINUX_COMPILE_TIME "21:18:51"
-#define LINUX_COMPILE_BY "felipe"
-#define LINUX_COMPILE_HOST "felipe-Parallels-Virtual-Platform"
-#define LINUX_COMPILE_DOMAIN "(none)"
-#define LINUX_COMPILER "gcc version 4.3.2 (Sourcery G++ Lite 2008q3-72) "
--- linux-2.6.35.3-17042011-km233/include/generated/utsrelease.h
+++
@@ -1,1 +0,0 @@
-#define UTS_RELEASE "2.6.35.3-433-g0fae922-ge94545f-dirty"
